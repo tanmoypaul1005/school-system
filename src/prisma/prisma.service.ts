143d454e0445a42c789b4d71c18fe47f18d7ad38
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-15 12:07:12 +0600

    feat: integrate Prisma ORM with NestJS application
    - Added Prisma dependencies to package.json for database interaction.
    - Created PrismaModule to provide PrismaService globally.
    - Implemented PrismaService to manage database connections and lifecycle.
    - Defined User model in schema.prisma for user management.
    - Updated AppService to include a method for fetching users from the database.
    - Configured Prisma with a PostgreSQL datasource in prisma.config.ts.

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+const adapter = new PrismaPg(pool);
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    await pool.end();
+  }
+}
