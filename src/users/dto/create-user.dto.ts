19407c42c517b487b2d97f5f1e63c846d5a4bf18
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-15 12:42:13 +0600

    feat: add user role management and validation with DTOs

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { Role } from '@prisma/client';
+import { IsEmail, IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsEnum(Role)
+  role?: Role;
+}
