6791fa2c832a2854ad231a58dd3fe0f0eab9a268
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-16 17:25:03 +0600

    feat: implement user creation functionality in user controller and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private readonly userService: UserService) {}
+
+    @Get()
+    async createUser(@Body() body: { name: string, email: string }) {
+        const { name, email } = body;
+        return this.userService.createUser(name, email);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly prisma:PrismaService) {}
+
+    async createUser(name: string, email: string) {
+        return this.prisma.user.create({
+            data: {
+                name,
+                email,
+            },
+        });
+    }
+}
