85ebcf7edf3941886c96b0b43b7b78438528d41f
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-17 21:18:34 +0600

    feat: add SectionController and update SectionModule to include it

--- src/section/section.module.ts
+++ src/section/section.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SectionService } from './section.service';
+import { SectionController } from './section.controller';
 import { SectionService } from './section.service';
 
 @Module({
-  providers: [SectionService]
+  providers: [SectionService],
+  controllers: [SectionController]
 })
 export class SectionModule {}
