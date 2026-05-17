bd2d415f47f754d039ad4bd4b03fd9bc39c9638a
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-17 21:12:39 +0600

    feat: add GET endpoint to ClassController for retrieving all classes

--- src/class/class.controller.ts
+++ src/class/class.controller.ts
@@ -1,10 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ClassService } from './class.service';
 
 @Controller('class')
 export class ClassController {
     constructor(private readonly classService: ClassService) {}
 
+    @Get('')
     async getAllClasses() {
         return this.classService.getAllClasses();
     }
