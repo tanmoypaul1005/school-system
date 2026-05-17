df16e7a61657762cfd556b481c64b4c2b2f3ee07
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-17 21:10:51 +0600

    feat: implement ClassController and ClassService with getAllClasses functionality

--- src/class/class.controller.ts
+++ src/class/class.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { ClassService } from './class.service';
 
 @Controller('class')
-export class ClassController {}
+export class ClassController {
+    constructor(private readonly classService: ClassService) {}
+
+    async getAllClasses() {
+        return this.classService.getAllClasses();
+    }
+}
--- src/class/class.service.ts
+++ src/class/class.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ClassService {}
+export class ClassService {
+    constructor(private readonly prisma:PrismaService) {}
+
+    async getAllClasses() {
+        return this.prisma.class.findMany({
+            include: {
+                students: true,
+                teacher: true,
+            },
+        });
+    }
+    
+}
