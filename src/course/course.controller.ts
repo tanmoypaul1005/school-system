085fb34a7bb8e3f3bdbcc4a6d049e87ae256e206
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-19 23:21:51 +0600

    feat: implement CourseController and CourseService with getAllCourses method

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CourseService } from './course.service';
 
 @Controller('course')
-export class CourseController {}
+export class CourseController {
+    constructor(private readonly courseService: CourseService) {}
+
+    @Get('')
+    async getAllCourses() {
+        return this.courseService.getAllCourses();
+    }
+}
--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async getAllCourses() {
+        return this.prisma.course.findMany({
+            include: {
+                section: true,
+            },
+        });
+    }
+}
