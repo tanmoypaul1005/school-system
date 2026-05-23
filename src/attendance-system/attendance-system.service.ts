f7999854d54783c144901fac6de7008762bfe55d
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-23 23:21:52 +0600

    fix: import PrismaService in AttendanceSystemService

--- src/attendance-system/attendance-system.service.ts
+++ src/attendance-system/attendance-system.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AttendanceSystemService {
-    
+    constructor(private readonly prisma:PrismaService) {}
 }
