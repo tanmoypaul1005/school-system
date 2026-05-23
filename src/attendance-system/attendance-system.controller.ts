8e284f8af95a86effa978ec6e386adfb41686d37
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-23 22:26:10 +0600

    feat: add AttendanceSystem module, controller, and service to the application

---
+++ src/attendance-system/attendance-system.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('attendance-system')
+export class AttendanceSystemController {}
---
+++ src/attendance-system/attendance-system.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AttendanceSystemController } from './attendance-system.controller';
+import { AttendanceSystemService } from './attendance-system.service';
+
+@Module({
+  controllers: [AttendanceSystemController],
+  providers: [AttendanceSystemService]
+})
+export class AttendanceSystemModule {}
