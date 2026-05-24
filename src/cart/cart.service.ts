2b72b310f4f27f6e6601a49a04d7f3d9370497db
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-24 22:21:12 +0600

    fix: update Cart and Product models to streamline cart management and remove CartItem DTO

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client/extension';
+import { CreateCartDto } from './dto/create-cart.dto';
 
 @Injectable()
 export class CartService {
     constructor(private readonly prisma:PrismaClient) {}
+
+    async createCart(createCartDto: CreateCartDto) {
+
+        const product=await this.prisma.product.findFirst({
+            where:{
+                id:createCartDto.productId,
+            }
+        });
+
+        return this.prisma.cart.create({
+            data: createCartDto,
+        });
+    }
 
     
 }
