46aef33d296e8a12ece9eca85da60e5130b06b9e
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-28 11:26:51 +0600

    fix: remove unnecessary blank line in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,7 @@ import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
+  
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
