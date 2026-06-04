214bfc16b90fa4468b9a88be1e5020586e3fabba
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-06-04 19:01:07 +0600

    fix: remove unnecessary blank line in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,6 @@ import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { VerifyOtpDto } from './dto/verify-otp.dto';
 import { LoginDto } from './dto/login.dto';
-
 @Controller('auth')
 
 export class AuthController {
